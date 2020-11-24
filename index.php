3294cd0ac9c729bc7213cb9921794b3486a676eb
Wara Massa <waramassa70@gmail.com>
2020-11-24 08:56:08 +0100

    php exo2

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// déclaration des variables 
+$lastName = 'Ahmed';
+$firstName = 'Ly';
+$age = 50;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP exo 2</title>
+</head>
+<body>
+    <!-- Affichage des variables -->
+    <p><?php echo $lastName; ?></p>
+    <p><?php echo $firstName; ?></p>
+    <p><?php echo $age; ?></p>
+</body>
+</html>
